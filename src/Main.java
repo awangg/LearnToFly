b650e76811c167821309e7c1f93540849b960c6a
andy_wang <andy_wang@student.wayland.k12a.ma.us>
2017-06-01 08:55:34 -0400

    Added Window

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import javax.swing.*;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Ha Yoto");
-        System.out.println("Hello World");
+public class Main extends JPanel {
+    final static int FRAMEWIDTH = 1200; final static int FRAMEHEIGHT = 600;
+    public static void main(String[] args) throws Exception {
+        JFrame window = new JFrame("Learn To Fly!");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
+
+        Main panel = new Main();
+        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
     }
 }
