5c7877be7ba3e4cce178ef6cbc5788329fe88fff
andrewward2001 <andrew_ward@student.wayland.k12.ma.us>
2017-06-02 07:57:22 -0400

    add savegetter

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import save.SaveGetter;
+
 import javax.swing.*;
+import java.io.File;
 
 public class Main extends JPanel {
-    final static int FRAMEWIDTH = 1200; final static int FRAMEHEIGHT = 600;
+    final static int FRAMEWIDTH = 1200, FRAMEHEIGHT = 600;
+    private static SaveGetter saveGetter;
     public static void main(String[] args) throws Exception {
+        saveGetter = new SaveGetter();
+
         JFrame window = new JFrame("Learn To Fly!");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
